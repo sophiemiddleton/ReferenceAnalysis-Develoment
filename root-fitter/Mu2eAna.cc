9ec4d3dcecc61fb45e96cdceef7a38179821bfec
sophiemiddleton <>
2024-04-18 19:10:10 -0500

    changed main

--- root-fitter/Mu2eAna.cc
+++ root-fitter/Mu2eAna.cc
@@ -8,15 +8,20 @@ Driving function for analysis
 
 using namespace std;
 
+void ImportNTuple(std::string filename){
+
+}
+
 void RunFit(){
-  std::cout<<" TODO "<<std::endl;
+  std::cout<<" will call the RooFit part of the code "<<std::endl;
+  //TODO - output yields of the fits as a tuple
 }
 
 int main(int argc, char* argv[]){
   std::cout<<"========== Welcome to Mu2eAna =========="<<std::endl;
   std::cout<<"-------- Analyzing "<<argv[1]<<"s ------------"<<std::endl;
 
-  TString livetime = argv[1]; // in seconds
+  TString filename = argv[1]; // TrkAna NTuple
   RunFit();
   return 0;
 }
