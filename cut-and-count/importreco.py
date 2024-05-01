141e241d91c4be35e7e0c83991456b3268c3804f
sophieMu2e <sophie@fnal.gov>
2024-05-01 11:13:56 -0500

    import works

---
+++ cut-and-count/importreco.py
@@ -0,0 +1,71 @@
+# imports NTupled data from reconstructed data/MC
+# date: 2024
+
+import awkward as ak
+import sys
+import uproot
+import pandas
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from cuts import *
+
+class ImportRecoData :
+
+    def __init__(self, fileName, treeName = "TrkAna", branchName = "trkana"):
+        """ Initialise the Class Object """
+        self.fileName = fileName
+        self.treeName = treeName
+        self.branchName = branchName
+        self.cuts = Cuts()
+
+    def Import_RecoFits(self, leafname, leafname_field):
+        """ import reconstructed trk ana"""
+        # import with uproot
+        trkana = uproot.open(self.fileName+":"+str(self.treeName)+"/"+str(self.branchName))
+        # find track fit branches for downstream (d) electron (em) loop helix (lh):
+        branches = trkana.arrays(filter_name=["/"+str(leafname)+"/", "/"+str(leafname_field)+"/"])
+        return branches
+
+    def PlotRecoMom(self, branches, low, hi ):
+        """ make basic reco mom plot, no position requirement """
+        branches['demfit_mom'] = np.sqrt((branches['demfit']['mom']['fCoordinates']['fX'])**2 + (branches['demfit']['mom']['fCoordinates']['fY'])**2 + (branches['demfit']['mom']['fCoordinates']['fZ'])**2)
+        fig, ax = plt.subplots(1,1)
+        n, bins, patches = ax.hist(ak.flatten(branches['demfit_mom'], axis=None), bins=50, range=(int(low), int(hi)), label='pass0a', histtype='step',color='b')
+        bin_centers = 0.5 * (bins[:-1] + bins[1:])
+        yerrs = []
+        for i, j in enumerate(n):
+          yerrs.append(math.sqrt(j))
+        plt.errorbar(bin_centers, n, yerr=np.sqrt(n), fmt='b.')
+        # add in style features:
+        ax.set_yscale('log')
+        ax.set_xlabel('Reconstructed Momentum (demfit_mom all) [MeV/c]')
+        ax.set_ylabel('Entries per bin')
+        ax.grid(True)
+        ax.legend()
+        plt.show()
+        
+        
+        
+    def PlotRecoMomEnt(self, branches, low, hi):
+        """ make basic reco mom plot, requirement for tracker entrance """
+        branches['demfit_mom'] = np.sqrt((branches['demfit']['mom']['fCoordinates']['fX'])**2 + (branches['demfit']['mom']['fCoordinates']['fY'])**2 + (branches['demfit']['mom']['fCoordinates']['fZ'])**2)
+        trk_ent_mask = (branches['demfit']['sid']==0)
+        
+        fig, ax = plt.subplots(1,1)
+        nEnt, binsEnt, patchesEnt = ax.hist(ak.flatten(branches[(trk_ent_mask)]['demfit_mom'], axis=None), bins=100, range=(int(low), int(hi)), label='ent fits', histtype='step',color='g')
+
+        bin_centersEnt = 0.5 * (binsEnt[:-1] + binsEnt[1:])
+        yerrsEnt = []
+        for i, j in enumerate(nEnt):
+          yerrsEnt.append(math.sqrt(j))
+        plt.errorbar(bin_centersEnt, nEnt, yerr=np.sqrt(nEnt), fmt='g.')
+
+        # add in style features:
+        ax.set_yscale('log')
+        ax.set_xlabel('Reconstructed Momentum (demfit_mom at ent) [MeV/c]')
+        ax.set_ylabel('Entries per bin')
+        ax.grid(True)
+        ax.legend()
+        plt.show()
+        
--- cut-and-count/main.py
+++ cut-and-count/main.py
@@ -8,73 +8,36 @@ import sys
 import argparse
 import math
 from optparse import OptionParser
-import matplotlib.pyplot as plt
-from ImportRecoData import ImportRecoData
+from importreco import ImportRecoData
+"""
 from ImportGenData import ImportGenData
 from Histograms import Histograms
 from Mu2eNegFunctions import *
 from Constants import *
 from Results import Results
-
+"""
 
 def main(options, args):
     print("=====================================================================")
     print("=====================================================================")
     print("=================== Mu2e Cut-and-count Analysis =====================")
-    print("=====================Written by S. Middleton=========================")
+    print("=================Written by Mu2e Analysis Tools Grp==================")
     print("=========================smidd@caltech.edu===========================")
     print("=====================================================================")
-    print("Users Options: CE", options.filename, options.treename, options.branchname)
+    #print("Users Options:", options.filelist, options.treename, options.branchname)
     print("===================calculating stats....please wait....==============")
 
-    # Pandas offers the most flexible analysis - arguments are used for window optimization only:
-    Run(100, options.mom_low, options.mom_high)
-    # TODO- add a UseROOT version
+    Run(options.mom_low, options.mom_hi)
 
-def Run(nbins, mom_low, mom_high,):
-
+def Run(mom_low, mom_high,):
     # Import the data into panadas dataframes:
-    recodata = ImportRecoData(options.filename, options.treename, otions.branchname)
-
-    # Make list of ROOT histograms for analysis (this step should be removed)
-    histos = Histograms(nbins, mom_low, mom_high,)
-
-    # Fille Reco dataframs (Note last arguement sets DoCuts - look at cuts.py for options)
-    DIO_reco_mom = recodata.GetFeature( "DIO", "deent.mom",False,True)
-    CE_reco_mom = recodata.GetFeature( "signal", "deent.mom",False,True)
-
-    # Fill Reco Hists:
-    histos.FillHistogram(histos.histo_CE_reconstructed , CE_reco_mom)
-    histos.FillHistogram(histos.histo_DIO_reconstructed_flat , DIO_reco_mom)
-
-    histos.DoDIOWeights(histos.histo_DIO_reconstructed_reweighted , DIO_reco_mom)
-
-    # Fill Generation dataframs:
-    DIO_gen_mom = gendata.GetFeature("DIO", "TMom")
-    CE_gen_mom = gendata.GetFeature("signal", "TMom")
-
-    # Fill Gen Hists:
-    histos.FillHistogram(histos.histo_CE_generated , CE_gen_mom)
-    histos.FillHistogram(histos.histo_DIO_generated_flat , DIO_gen_mom)
-
-    histos.DoDIOWeights(histos.histo_DIO_generated_reweighted , DIO_gen_mom)
-
-    # Import constants - this is were we store nPOT, capture/decay rates etc.
-    # Edit the constants if you have a new design to reflect that.
-    constants = Constants()
-
-    # Build Mu2e yields Functions:
-    yields = YieldFunctions(mom_low, mom_high, constants)
-
-    # Fill Results i.e. optimial window (requires work)
-    yields.FillResults()
-
-    # Get SES/BFUL/Eff for fixed signal window:
-    yields.GetSingleResult(constants.fixed_window_lower, constants.fixed_window_upper)
-
-    # Save plots:
-    yields.WriteHistograms()
-
+    recodata = ImportRecoData(options.filelist, options.treename, options.branchname)
+    # extract reco mom for Loop Helix:
+    branches = recodata.Import_RecoFits("demfit", "demlh")
+    # make a histogram of this:
+    recodata.PlotRecoMom(branches, options.mom_low, options.mom_hi)
+    recodata.PlotRecoMomEnt(branches, options.mom_low, options.mom_hi)
+    
 if __name__ == "__main__":
 
     parser = OptionParser()
