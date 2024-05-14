e66f0fc9d181c1bcb1f9b1e201f0c9ad2f211727
sophieMu2e <sophie@fnal.gov>
2024-05-14 08:32:32 -0500

    name change

--- cut-and-count/histograms.py
+++
@@ -1,4 +0,0 @@
-# Author : S Middleton
-# Date : 2020
-# Purpose : class to store histograms
-#TODO - this class requires work!
--- cut-and-count/optimizer.py
+++
@@ -1,2 +0,0 @@
-# TODO -optimize cuts in mom and time using MC, then apply to data
-# FIXME - understand how to incoperate the fact that data = MC currently
